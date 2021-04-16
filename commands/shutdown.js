9bb171e7d6343c30b5f7ada5ca7c224ece1650e4
ZayHubOfficial <81226149+ZayHubOfficial@users.noreply.github.com>
2021-04-17 01:52:49 +0530

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,7 +4,8 @@ module.exports = {
     execute(message, args){
 
         if(message.member.roles.cache.has('823777643551129611')){
-              logchannel.send(`<@${'710904756138147882'}> Shutting Down...`);
+            const logchannel = message.guild.channels.cache.find(channel => channel.id === '832546560096206898')  
+            logchannel.send(`<@${'710904756138147882'}> Shutting Down...`);
     }   else {
         message.channel.send('You do not have permission to run this command');
     }
