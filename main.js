50edb244ba06144520316ef5c30032cfd4b7f913
ZayHubOfficial <81226149+ZayHubOfficial@users.noreply.github.com>
2021-04-18 18:26:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  
 const prefix = '-';
- 
+ //change
 const fs = require('fs');
- 
 client.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
