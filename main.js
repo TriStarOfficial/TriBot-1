ee0d593e9ad3943165f1a0b24b6d6f43b4e58801
ZayHubOfficial <81226149+ZayHubOfficial@users.noreply.github.com>
2021-04-18 21:15:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  
 const prefix = '-';
- //change
 const fs = require('fs');
 client.commands = new Discord.Collection();
  
