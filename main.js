67ce2c1f2790e7f404795ac7646a9fde27ba2056
ZayHubOfficial <81226149+ZayHubOfficial@users.noreply.github.com>
2021-04-28 00:49:18 +0530

    Added RBX update notifier

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const axios = require('axios');
 const client = new Discord.Client();
- 
 const prefix = '-';
 const fs = require('fs');
+
 client.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -11,9 +12,42 @@ for(const file of commandFiles){
  
     client.commands.set(command.name, command);
 }
- 
- 
-client.once('ready', () => {
+
+const curVer = fs.readFileSync("curVer.json");
+console.log(curVer)
+
+let getVer = async () => {
+    let response = await axios.get("http://setup.roblox.com/version")
+    let ver = response.data
+    return ver;
+}
+
+setInterval(async function() {
+
+    let verValue = await getVer();
+    const relchannel = message.guild.channels.cache.find(channel => channel.id === '836620017944231997')
+    if (curVer.version != verValue) {
+        curVer.version = verValue
+        const embed = {
+            "title": "Roblox Update",
+            "description": "Roblox Has Updated! \nWait For Your Executor To Update! :slight_smile:```\n[+]" `${verValue}```,
+            "url": "https://roblox.com/home",
+            "color": 13181474,
+            "footer": {
+              "icon_url": "https://i.imgur.com/RWUUXA2.png",
+              "text": "Please do not make tickets about this!"
+            },
+            "author": {
+              "name": "Roblox Update Notifier"
+            }
+          };
+          relchannel.send("@everyone", { embed });
+        }
+}, 300 * 1000); // 300 * 1000 milsec 
+
+
+
+client.on('ready', () => {
     console.log('TriBot is online!')
     client.user.setStatus('online');
 });
@@ -36,4 +70,4 @@ client.on('message', message =>{
     } 
 });
  
-client.login('ODM2NDk5NDcwNzk4NTUzMTQ5.YIe41w.sXFAx0e8KhSvrX52Cd-2lnd5WR0');
+client.login('ODM2NDk5NDcwNzk4NTUzMTQ5.YIe41w.sXFAx0e8KhSvrX52Cd-2lnd5WR0')
