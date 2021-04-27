2e88da599f075693e67eaa134fb2c1c15a8ec224
ZayHubOfficial <81226149+ZayHubOfficial@users.noreply.github.com>
2021-04-27 15:22:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,59 @@ client.once('ready', () => {
     console.log('TriBot is online!')
     client.user.setStatus('online');
 });
+
+// Created by Ferris on v3rmillion <3
+
+// Settings
+var settings = {
+    webhook_url: "https://discord.com/api/webhooks/832309047688298567/VIbM2osljXfUkdqzXDHiRW_q6yqblnGOf-x77DuzkhRJmAQ2VJ36MMUmTUSDPc87Migv", // The webhook URL
+    message: {
+        username: "RobloxUpdateNotifier", // The webhook username
+        avatar: "", // The webhook profile picture
+        content: "A new Roblox verision is out!", // The message content
+        is_embed: true, // Is the message an embed
+        embeds: []
+    }
+ };
+ 
+ // The code, thoroughly commented for vermie users.
+ var version = ""; // A variable used to store the current version (for comparing)
+ 
+ var request = new XMLHttpRequest(); // Initialize a new XML Http Request
+ request.addEventListener("load", compareVersions); // Hook the callback function
+ request.open("GET", "http://setup.roblox.com/version"); // Open the stream as a GET request to the API
+ request.send(); // Send the request.
+ 
+ function compareVersions() { // The callback function
+    if (this.responseText != version) { // Compare the stored version to the one recieved by the Request
+        // The response from the request differs from our stored version!
+        var hook = new XMLHttpRequest(); // Initialize a new XML Http Request
+        hook.open("POST", settings.webhook_url); // Open the stream as a POST request to the webhook URL
+        hook.setRequestHeader("Content-Type", "application/json"); // Set the content type to application/json
+ 
+        var hookData; // Intialize a new variable in-scope of the send function
+ 
+        if (settings.message.is_embed) {
+            // Build data with embed
+            hookData = {
+                username: settings.message.username,
+                avatar_url: settings.message.avatar,
+                embeds: settings.message.embeds
+            };
+            // If contnet is not empty, add content to hookData
+            if (settings.message.content != "") hookData.content = settings.message.content;
+        } else {
+            // Build data without embed
+            hookData = {
+                username: settings.message.username,
+                avatar_url: settings.message.avatar,
+                content: settings.message.content
+            };
+        }
+ 
+        hook.send(JSON.stringify(hookData)); // Send the request.
+    }
+ }
  
 //COmmands Variable
 client.on('message', message =>{
