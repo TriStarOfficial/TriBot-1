4bc56b942ecc1ae79b88829456e592d188bbc7bb
MaiKokain <>
2021-05-31 02:57:43 +0000

    Deleted Web

--- Function/Server/ser.js
+++ Function/Server/ser.js
@@ -5,9 +5,6 @@ const server = express();
 server.all('/', (req, res)=>{
     res.send('Your bot is alive!')
 })
-server.all('/script', (req, res) => {
-    res.sendFile(path.join(__dirname), './Web/script.html')
-})
 function keepAlive(){
     server.listen(3000, ()=>{console.log("Server is Ready! ✅")});
 }
