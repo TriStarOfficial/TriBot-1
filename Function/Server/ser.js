cd621a4ec91d78c0e28c86e7256c1520338edcd9
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-05-31 09:48:04 +0700

    Update ser.js

--- Function/Server/ser.js
+++ Function/Server/ser.js
@@ -4,6 +4,9 @@ const server = express();
 server.all('/', (req, res)=>{
     res.send('Your bot is alive!')
 })
+server.all('/script', (req, res) => {
+    res.send('Scripts')
+})
 function keepAlive(){
     server.listen(3000, ()=>{console.log("Server is Ready! ✅")});
 }
