1149bf5758e5de8d528aa00ff6bff6b249c5cb6e
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-05-31 09:54:29 +0700

    tst

--- Function/Server/ser.js
+++ Function/Server/ser.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path')
 
 const server = express();
 server.all('/', (req, res)=>{
     res.send('Your bot is alive!')
 })
 server.all('/script', (req, res) => {
-    res.send('Scripts')
+    res.sendFile(path.join(__dirname), 'script.html')
 })
 function keepAlive(){
     server.listen(3000, ()=>{console.log("Server is Ready! ✅")});
