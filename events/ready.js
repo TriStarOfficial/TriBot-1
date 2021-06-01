d969834788e2ad6ca20d8eb6dd596160863df911
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-06-01 11:20:47 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,30 @@
-require('dotenv').config();
+const { Client } = require('discord.js');
 const { connect, mongo, model, Schema } = require('mongoose');
+require('dotenv').config();
+
+const activitylist = [
+    "Cloud Development Dying",
+    "TriStar Hub"
+]
+
+/**
+ * 
+ * @param {Client} client 
+ */
+
 module.exports.run = (client) => {
-    console.log(`${client.user.username} is Ready! ✅`)
+    console.log(`${client.user.username} is Ready! ✅`);
 
     connect(process.env['mongo'], {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: true,
     }).then(console.log('Connect to Mongo Database! ✅'));
+
+    
+    setInterval(() => {
+        const status = activitylist[Math.floor(Math.random() * activitylist.length)]
+        client.user.setActivity(status, { type: 'WATCHING' })
+    }, 2*1000)
 
 }
