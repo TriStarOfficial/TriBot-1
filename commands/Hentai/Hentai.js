06b7084c0427b4332dd173d999e8b07f27c25c10
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-06-06 16:34:06 +0700

    Update Hentai.js

--- commands/Hentai/Hentai.js
+++ commands/Hentai/Hentai.js
@@ -1,8 +1,18 @@
-const { Message, Client, MessageEmbed } = require('discord.js');
-const { MessageButton } = require('discord-buttons');
+const {
+    Message,
+    Client,
+    MessageEmbed
+} = require('discord.js');
+const {
+    MessageButton
+} = require('discord-buttons');
+const {
+    default: axios
+} = require('axios');
 const CurrentArgs = [
     "game",
 ]
+const RandomInt = require('../../Modules/RandomInt')
 
 module.exports = {
     name: 'hentai',
@@ -19,6 +29,7 @@ module.exports = {
      * @param {String[]} args 
      */
     execute: async (client, message, args, text, prefix, command) => {
+        var Int = RandomInt(50)
         var count;
         const ErrorEmbed = new MessageEmbed()
         ErrorEmbed.setTitle('Missing Argument')
@@ -26,11 +37,32 @@ module.exports = {
             ErrorEmbed.addField(CurrentArgs[count], "nil", true)
         }
         ErrorEmbed.setColor('RED')
-        if (!args.slice(0).join(" ")) return message.channel.send(ErrorEmbed) 
+        if (!args.slice(0).join(" ")) return message.channel.send(ErrorEmbed)
         switch (args.slice(0).join(" ").toLowerCase()) {
             case "game":
-                message.channel.send('<@722647978577363026> Send more game white nigba!')
+                const rbxres = await axios.get('https://games.roblox.com/v1/games/list?model.maxRows=50', {
+                    method: 'GET',
+                    headers: {
+                        'Accpet': 'application/json'
+                    }
+                }).then(res => res.data);
+                const GameData = rbxres['games'][Int]
+
+                const GameEmbed = new MessageEmbed()
+                    .setTitle(GameData.name)
+                    .setColor('BLURPLE')
+                    .setDescription(GameData.gameDescription)
+                    .addFields([{
+                        name: 'Creator Data',
+                        value: `Creator Name: ${GameData.creatorName}\nCreator ID: ${GameData.creatorId}`,
+                        inline: true
+                    }])
+                    .setURL(`https://roblox.com/games/${GameData.placeId}`)
+                    .setFooter('Auto Generated Via Roblox API!')
+                message.channel.send('<@722647978577363026> send more game white nigga', { embed: GameEmbed })
             break;
+
+            
         }
     }
 }
