37a2459db6e2d1a236ad62809997d6218c11c148
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-06-06 20:05:57 +0700

    Changes HTML and CSS

--- Function/Server/ser.js
+++ Function/Server/ser.js
@@ -2,13 +2,19 @@ const express = require('express');
 const path = require('path')
 
 const server = express();
+
+server.use('/public', express.static('public'))
+
 server.all('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "html/card.html"))
+    res.sendFile(path.join(__dirname, "../../public/card.html"))
 })
 server.all('/discord', (req, res) => {
     res.redirect('https://discord.gg/MkBuu8Xpk3')
 })
 function keepAlive(){
-    server.listen(3000, ()=>{console.log("Server is Ready! | ✅")});
+    const listener = server.listen(3000, () => {
+        console.log(`Listening to port 127.0.0.1:${listener.address().port}`);
+    })
 }
+
 module.exports = keepAlive;
