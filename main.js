5e2b571d94a2779c5d0e320f3067b0a78466d4c3
MaiKokain <71884625+MaiKokain@users.noreply.github.com>
2021-06-17 09:45:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -48,34 +48,37 @@ client.on('message', async message => {
 })
 client.on('message', async message => {
     if (message.content.includes('streancommunuty.ru')) {
-        message.delete()
-        function DMMessage(id) {
-            const Embed = new Discord.MessageEmbed()
-            .setColor('BLURPLE')
-            .setDescription(`You're not allowed to say that word! <:peperules:853618792583266304>`)
-
-            client.users.cache.get(id).send(Embed).catch(err => {
-                if (err) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('Unable to DM User!')).then(m => m.delete({ timeout: 1000*5 }))
-            })
-        }
-        DMMessage(message.author.id)
-
-        
-        
-        // function BanLogs(id) {
+        message.member.ban({
+            days: 7,
+            reason: 'Auto Mod'
+        });
+        // function DMMessage(id) {
         //     const Embed = new Discord.MessageEmbed()
         //     .setColor('BLURPLE')
-        //     .setTitle('Auto Ban!')
-        //     .setDescription('Fake Steam Trade URL was Detected by TriBot!')
-        //     .addField('User Info', `
-        //     > Name & ID: ${message.author} | ${message.author.tag} | ${message.author.id}
-        //     > Joined At: ${message.member.joinedAt}
-        //     > Created At: ${message.author.createdAt}
-        //     `)
-        //     client.channels.cache.get(id).send(Embed)
-        // }
+        //     .setDescription(`You're not allowed to say that word! <:peperules:853618792583266304>`)
 
-        // BanLogs("846666365863067688")
+        //     client.users.cache.get(id).send(Embed).catch(err => {
+        //         if (err) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription('Unable to DM User!')).then(m => m.delete({ timeout: 1000*5 }))
+        //     })
+        // }
+        // DMMessage(message.author.id)
+
+        
+        
+        function BanLogs(id) {
+            const Embed = new Discord.MessageEmbed()
+            .setColor('BLURPLE')
+            .setTitle('Auto Ban!')
+            .setDescription('Fake Steam Trade URL was Detected by TriBot!')
+            .addField('User Info', `
+            > Name & ID: ${message.author} | ${message.author.tag} | ${message.author.id}
+            > Joined At: ${message.member.joinedAt}
+            > Created At: ${message.author.createdAt}
+            `)
+            client.channels.cache.get(id).send(Embed)
+        }
+
+        BanLogs("846666365863067688")
     }
 });
 client.on('message', async msg => {
